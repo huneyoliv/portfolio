02bfefb0ff67d9236a8fca62238798e56f982841
Huney Oliveira <huney.oliveira@gmail.com>
2026-04-10 16:43:12 -0300

    feat: add site configuration constants for portfolio metadata and profile details

--- src/config.ts
+++ src/config.ts
@@ -11,45 +11,7 @@ export const siteConfig = {
   },
   aboutMe:
     "Comecei a estudar programação em 2019, inicialmente como uma brincadeira, movido pela curiosidade de entender o que acontecia por trás das telas. Com o tempo, essa curiosidade se transformou em paixão e dedicação. Desde então, venho me especializando em desenvolvimento Back-End, com foco na criação de APIs robustas, na implementação de arquiteturas de microsserviços e na otimização de sistemas de alta performance. Atualmente, estou em processo de transição para a área de Ciência de Dados, unindo minha experiência em desenvolvimento à análise e interpretação de dados.",
-  skills: [
-    "JavaScript",
-    "TypeScript",
-    "Python",
-    "Node.js",
-    "FastAPI",
-    "PostgreSQL",
-    "MongoDB",
-    "Redis",
-    "MySQL",
-    "Docker",
-    "AWS",
-    "Git",
-  ],
-  projects: [
-    {
-      name: "TG Cloner",
-      description:
-        "Aplicação GUI feita para o envio e cópia de grupos do Telegram.",
-      link: "https://github.com/huneyoliv/tg-cloner",
-      skills: ["Python", "FastAPI"],
-    },
-  ],
-  education: [
-    {
-      school: "Universidade Federal de Sergipe (UFS)",
-      degree: "Bacharelado em Ciência da Computação",
-      dateRange: "2026 - 2031",
-      achievements: [
-        "Formação sólida em algoritmos, estruturas de dados, engenharia de software e sistemas distribuídos.",
-      ],
-    },
-    {
-      school: "Universidade Tiradentes (UNIT)",
-      degree: "Bacharelado em Sistemas de Informação",
-      dateRange: "2025 - 2026 (Interrompido)",
-      achievements: [
-        "Formação sólida em algoritmos, estruturas de dados, engenharia de software e sistemas distribuídos.",
-      ],
-    },
-  ],
+  skills: [],
+  projects: [],
+  education: [],
 };
